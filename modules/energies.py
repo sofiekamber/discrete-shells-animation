aa91cf4132fdc2035092397c3fe240d07e8b9c38
JasminKoller <kollerja@ethz.ch>
2023-11-16 16:54:17 +0100

    create stretching-area energy

--- modules/energies.py
+++ modules/energies.py
@@ -6,10 +6,34 @@ def stretch_edge(k_stretch:ti.f64):
     """This function computes the energy E_L resulting from change of edge lengths"""
     pass
 
+@ti.func
+def tri_area(x, id0, id1, id2):
+    """Returns triangle area of given vertices
+    """
+    term1 = ((x[id0, 1] - x[id1, 1]) * (x[id0, 2] - x[id2, 2])) - (
+            (x[id0, 2] - x[id1, 2]) * (x[id0, 1] - x[id2, 1]))
+    term2 = ((x[id0, 2] - x[id1, 2]) * (x[id0, 0] - x[id2, 0])) - (
+            (x[id0, 0] - x[id1, 0]) * (x[id0, 2] - x[id2, 2]))
+    term3 = ((x[id0, 0] - x[id1, 0]) * (x[id0, 1] - x[id2, 1])) - (
+            (x[id0, 1] - x[id1, 1]) * (x[id0, 0] - x[id2, 0]))
+
+    area = 0.5 * ti.sqrt(ti.pow(term1, 2) + ti.pow(term2, 2) + ti.pow(term3, 2))
+    return area
+
 @ti.kernel
-def stretch_area(k_area:ti.f64):
+def stretch_area(x_undef:ti.template(), x_def:ti.template(), indices:ti.template(), num_triangles: int, k_area:ti.f64):
     """This function computes the energy E_A resulting from change of triangle areas"""
-    pass
+    E_A = 0.0
+    for tri_id in range(num_triangles):
+        #compute area of each triangle
+        id0 = indices[tri_id * 3 + 0]
+        id1 = indices[tri_id * 3 + 1]
+        id2 = indices[tri_id * 3 + 2]
+        area_undef = tri_area(x_undef, id0, id1, id2)
+        area_def = tri_area(x_def, id0, id1, id2)
+        E_A += area_undef * ti.pow(((area_def/area_undef) - 1),2)
+
+    return k_area * E_A
 
 
 @ti.kernel
