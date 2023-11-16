1c99acb7e987bc648954ca567da3d76b117fa0ce
JasminKoller <kollerja@ethz.ch>
2023-11-16 14:59:32 +0100

    add energy params

--- modules/energies.py
+++ modules/energies.py
@@ -2,17 +2,17 @@
 import taichi as ti
 
 @ti.kernel
-def stretch_edge():
+def stretch_edge(k_stretch):
     """This function computes the energy E_L resulting from change of edge lengths"""
     pass
 
 @ti.kernel
-def stretch_area():
+def stretch_area(k_area):
     """This function computes the energy E_A resulting from change of triangle areas"""
     pass
 
 
 @ti.kernel
-def bending_angles():
+def bending_angles(k_bend):
     """This function computes the bending E_B resulting from change of dihedral angles"""
     pass
