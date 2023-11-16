97bac6eda7cfd26826dbb6d95aab21c54bac3144
JasminKoller <kollerja@ethz.ch>
2023-11-16 15:02:34 +0100

    fix energy typing

--- modules/energies.py
+++ modules/energies.py
@@ -2,17 +2,17 @@
 import taichi as ti
 
 @ti.kernel
-def stretch_edge(k_stretch):
+def stretch_edge(k_stretch:ti.f64):
     """This function computes the energy E_L resulting from change of edge lengths"""
     pass
 
 @ti.kernel
-def stretch_area(k_area):
+def stretch_area(k_area:ti.f64):
     """This function computes the energy E_A resulting from change of triangle areas"""
     pass
 
 
 @ti.kernel
-def bending_angles(k_bend):
+def bending_angles(k_bend:ti.f64):
     """This function computes the bending E_B resulting from change of dihedral angles"""
     pass
