feef9e07fd5774f1ba43acb1b635f14839bc0907
sofiekamber <sofie.kamber@gmail.com>
2023-11-16 09:42:00 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import taichi as ti
+ti.init(arch=ti.vulkan)  # Alternatively, ti.init(arch=ti.cpu)
+
+n = 128
+quad_size = 1.0 / n
+dt = 4e-2 / n
+substeps = int(1 / 60 // dt)
+
+x = ti.Vector.field(3, dtype=float, shape=(n, n))
+v = ti.Vector.field(3, dtype=float, shape=(n, n))
+
+num_triangles = (n - 1) * (n - 1) * 2
+indices = ti.field(int, shape=num_triangles * 3)
+vertices = ti.Vector.field(3, dtype=float, shape=n * n)
+
+
+@ti.kernel
+def initialize_mesh_indices():
+    for i, j in ti.ndrange(n - 1, n - 1):
+        quad_id = (i * (n - 1)) + j
+        # 1st triangle of the square
+        indices[quad_id * 6 + 0] = i * n + j
+        indices[quad_id * 6 + 1] = (i + 1) * n + j
+        indices[quad_id * 6 + 2] = i * n + (j + 1)
+        # 2nd triangle of the square
+        indices[quad_id * 6 + 3] = (i + 1) * n + j + 1
+        indices[quad_id * 6 + 4] = i * n + (j + 1)
+        indices[quad_id * 6 + 5] = (i + 1) * n + j
+
+
+initialize_mesh_indices()
+
+
+@ti.kernel
+def update_vertices():
+    for i, j in ti.ndrange(n, n):
+        vertices[i * n + j] = x[i, j]
+
+window = ti.ui.Window("Discrete Shells", (1024, 1024), vsync=True)
+canvas = window.get_canvas()
+canvas.set_background_color((1, 1, 1))
+scene = ti.ui.Scene()
+camera = ti.ui.Camera()
+
+current_t = 0.0
+
+while window.running:
+    if current_t > 1.5:
+        # Reset
+        current_t = 0
+
+    for i in range(substeps):
+        # substep()
+        current_t += dt
+    update_vertices()
+
+    camera.position(0.0, 0.0, 3)
+    camera.lookat(0.0, 0.0, 0)
+    scene.set_camera(camera)
+
+    scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
+    scene.ambient_light((0.5, 0.5, 0.5))
+    scene.mesh(vertices,
+               indices=indices,
+               per_vertex_color=colors,
+               two_sided=True)
+
+    canvas.scene(scene)
+    window.show()
