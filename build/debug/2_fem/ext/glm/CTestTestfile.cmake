2d1a0d40314d288731f497e4161862f7b0f0e437
JasminKoller <101703037+JasminKoller@users.noreply.github.com>
2023-11-25 16:09:04 +0100

    Delete build/debug directory

--- build/debug/2_fem/ext/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ETH/Master/PhysicallyBasedSimulationInCG/pbs23-solution/2_fem/ext/glm
-# Build directory: D:/ETH/Master/PhysicallyBasedSimulationInCG/pbs23-solution/build/debug/2_fem/ext/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
